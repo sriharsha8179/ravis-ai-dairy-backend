1960aa0a6d5712dc7aa777eab9c825473da94b35
sriharsha8179 <sriharsha8179@gmail.com>
2026-02-26 14:41:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from database import engine
 import models
-from routes.milk import router as milk_router
-from routes.farmer import router as farmer_router
-from routes.auth_routes import router as auth_router
+from milk import router as milk_router
+from farmer import router as farmer_router
 from auth import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,5 +16,4 @@ app.include_router(farmer_router)
 
 @app.get("/")
 def root():
-
     return {"message": "Dairy SaaS Running Successfully"}
