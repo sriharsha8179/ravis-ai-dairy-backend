ab77bd115e17a547de7ec4d76149e3903e2f4031
sriharsha8179 <sriharsha8179@gmail.com>
2026-02-26 15:35:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from routes.auth_routes import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Dairy Management SaaS")
+app = FastAPI(
+    title="Dairy Management SaaS",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
 
 app.include_router(auth_router)
 app.include_router(milk_router)
@@ -17,3 +22,4 @@ app.include_router(farmer_router)
 @app.get("/")
 def root():
     return {"message": "Dairy SaaS Running Successfully"}
+
