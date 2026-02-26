3ea5ce97d9c82759e1c879547e8c192f82d28f5c
sriharsha8179 <sriharsha8179@gmail.com>
2026-02-26 15:22:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from database import engine
 import models
-from milk import router as milk_router
-from farmer import router as farmer_router
-from auth_routes import router as auth_router
+from routes.milk import router as milk_router
+from routes.farmer import router as farmer_router
+from routes.auth_routes import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 
